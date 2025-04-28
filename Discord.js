2f11b4b294d13bf69a4aea449afaa390c765098e
DarkarZT <116039912+DarkarZT@users.noreply.github.com>
2025-04-27 20:49:28 -0500

    Update Discord.js

--- Discord.js
+++ Discord.js
@@ -1,14 +1,4 @@
 // ✅ Dependencias principales
-const { exec } = require('child_process');
-
-exec('ffmpeg -version', (error, stdout, stderr) => {
-  if (error) {
-    console.error(`exec error: ${error}`);
-    return;
-  }
-  console.log(`stdout: ${stdout}`);
-  console.log(`stderr: ${stderr}`);
-});
 
 const { Client, GatewayIntentBits, Partials } = require('discord.js');
 const { DisTube } = require('distube');
